b2e05664d7e822fd3e45724d8967218e2b327d0c
josephdlawson21 <josephdlawson21@gmail.com>
2018-01-08 10:35:12 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,8 @@ class Application
 
   @@items = []
 
+  Application.class_variable_set(:@@items, [Item.new("Figs",3.42),Item.new("Pears",0.99)])
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -9,9 +11,9 @@ class Application
     if req.path.match(/items/)
       search_term = req.path.split('/').last
       if @@items.select {|thing| thing.name == search_term} != []
-        item = @@items.select {|thing| thing.name == search_term}
+        item = @@items.find {|thing| thing.name == search_term}
         # item[0].price
-        resp.write item
+        resp.write item.price
       else
         resp.write "Item not found"
         resp.status = 400
