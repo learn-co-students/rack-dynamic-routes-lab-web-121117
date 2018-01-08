fca40a1aaaa0bb591c2e58143a15b226554bcbf9
github username <github email address>
2018-01-08 09:15:31 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,17 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items/<ITEM NAME>"
-      resp.write "You requested the songs"
+
+    if req.path.match(/items/)
+      search_term = req.path.split("/items/").last
+      item = Item.all.find{|item| item.name == search_term}
+      if item
+        # item = Item.all.find(search_term)
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
